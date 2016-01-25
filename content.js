7c3d94c2c80665c24953ed5e28f468ce7fef8071
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-24 22:39:23 -0800

    note to self: $ != jQuery

--- content.js
+++ content.js
@@ -16,7 +16,10 @@ InboxSDK.load('1', 'sdk_WerewolfPost_92a6a5c025').then(function(sdk){
         //Do Not Store Sent Copies In Sent Mail. Delete With Prejudice.
         var combinedRecipients = composeView.getToRecipients() + composeView.getCcRecipients() + composeView.getBccRecipients();
 
-        console.log(event.dropdown.el);
+        var el = event.dropdown.el
+        //apparently I don't have jQuery by default, but $('.selector') still
+        //works in the Chrome Dev console 
+        console.log(el);
       },
     });
   });
