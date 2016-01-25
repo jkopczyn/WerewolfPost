0e1aa5bbeeb80188aa940c27400d80ff2978b64d
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-24 21:23:42 -0800

    dropdown for percentage

--- content.js
+++ content.js
@@ -5,13 +5,18 @@ InboxSDK.load('1', 'sdk_WerewolfPost_92a6a5c025').then(function(sdk){
     // a compose view has come into existence, do something with it!
     composeView.addButton({
       title: "Compose Werewolf Message",
-      iconUrl: "http://icons.iconarchive.com/icons/icons8/halloween/128/werewolf-icon.png",
+      iconUrl: "https://icons.iconarchive.com/icons/icons8/halloween/128/werewolf-icon.png",
+      hasDropdown: true,
+      type: "SEND_ACTION",
       onClick: function(event) {
         //store a copy with the werewolf postscript
         //for each recipient, generate a random number
-        //and if hits threshold - ASSIGNED HOW? - make 
+        //and if hits threshold - assigned in dropdown - make 
         //a copy and send it to that recipient.
-        event.composeView.ACTION();
+        //Do Not Store Sent Copies In Sent Mail. Delete With Prejudice.
+        var combinedRecipients = composeView.getToRecipients() + composeView.getCcRecipients() + composeView.getBccRecipients();
+
+        console.log(event.dropdown.el);
       },
     });
   });
