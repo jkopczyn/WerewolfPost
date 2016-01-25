45a9a511e8c0f310db044be3501553e9c02d4e54
Jacob Kopczynski <ja.kopczynski@gmail.com>
2016-01-24 19:57:03 -0800

    using the Inbox SDK

--- content.js
+++ content.js
@@ -1,17 +1,18 @@
-InboxSDK.load('1', 'Hello World!').then(function(sdk){
+InboxSDK.load('1', 'sdk_WerewolfPost_92a6a5c025').then(function(sdk){
 
-	// the SDK has been loaded, now do something with it!
-	sdk.Compose.registerComposeViewHandler(function(composeView){
-//
-//		// a compose view has come into existence, do something with it!
-//		composeView.addButton({
-//			title: "My Nifty Button!",
-//			iconUrl: 'https://lh5.googleusercontent.com/itq66nh65lfCick8cJ-OPuqZ8OUDTIxjCc25dkc4WUT1JG8XG3z6-eboCu63_uDXSqMnLRdlvQ=s128-h128-e365',
-//			onClick: function(event) {
-//				event.composeView.insertTextIntoBodyAtCursor('Hello World!');
-//			},
-//		});
-//
-	});
+  sdk.Compose.registerComposeViewHandler(function(composeView){
 
+    // a compose view has come into existence, do something with it!
+    composeView.addButton({
+      title: "Compose Werewolf Message",
+      iconUrl: "http://icons.iconarchive.com/icons/icons8/halloween/128/werewolf-icon.png",
+      onClick: function(event) {
+        //store a copy with the werewolf postscript
+        //for each recipient, generate a random number
+        //and if hits threshold - ASSIGNED HOW? - make 
+        //a copy and send it to that recipient.
+        event.composeView.ACTION();
+      },
+    });
+  });
 });
